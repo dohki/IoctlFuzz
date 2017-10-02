d324287c2ec1b99d528b8a0a5cbc7d5bda7105f4
DoHyeok Kim <illuxic@gmail.com>
2017-10-03 05:08:02 +0900

    Complete on_target/setup.py

--- on_target/setup.py
+++ on_target/setup.py
@@ -1,14 +1,23 @@
-import winreg
+import os
+import winshell
+
+def make_line(string):
+        return '{}\n'.format(string)
 
 def add_script_to_startup_dir():
-	# TODO: Where SHGFP_TYPE_CURRENT or DEFAULT are defined?
-	startup_dir_path = win32com.shell.shell.SHGetFolderPath(None, win32com.shell.shellcon.CSIDL_STARTUP, None, 0)
-	script_path = os.path.join(startup_dir_path, 'on_target_boot.bat')
+	script_path = os.path.join(winshell.startup(), 'on_target_boot.bat')
 
 	with open(script_path, 'w') as f:
 		cmds  = make_line('pushd {}'.format(os.getcwd()))
 		cmds += make_line('start "" python ioctl_fuzzer.py')
 		f.write(cmds)
 
+def create_ioctl_items_dir():
+        try:
+                os.mkdir('ioctl_items')
+        except FileExistsError:
+                pass
+
 if __name__ == '__main__':
 	add_script_to_startup_dir()
+	create_ioctl_items_dir()
