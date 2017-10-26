5654b7b3841a3907cb36543deef3e34f81c8e46d
DoHyeok Kim <dohyeokkim@postech.edu>
2017-10-26 17:24:57 +0900

    Add a feature for handling errors

--- on_target/src/fuzzer.py
+++ on_target/src/fuzzer.py
@@ -26,47 +26,54 @@ def print_status():
 	run_time    = datetime.timedelta(seconds=time.time() - start_time)
 	print(STATUS.format(tries, run_time), end='\r')
 
-def print_error(): 
-	
-	def get_fmt_msg_flag():
-		FORMAT_MESSAGE_ALLOCATE_BUFFER	= 0x00000100
-		FORMAT_MESSAGE_FROM_SYSTEM		= 0x00001000
-		FORMAT_MESSAGE_IGNORE_INSERTS	= 0x00000200
+def handle_err():
 
-		return FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS
-
-	def get_lcid_eng():
+	def print_err(err_code): 
 		
-		def MAKELANGID(primary_lang, sub_lang):
-			return (primary_lang & 0xff) | (sub_lang & 0xff) << 10
+		def get_fmt_msg_flag():
+			FORMAT_MESSAGE_ALLOCATE_BUFFER	= 0x00000100
+			FORMAT_MESSAGE_FROM_SYSTEM		= 0x00001000
+			FORMAT_MESSAGE_IGNORE_INSERTS	= 0x00000200
 
-		LANG_ENGLISH		= 0x09
-		SUBLANG_ENGLISH_US	= 0x01
-		LCID_ENGLISH		= MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US)
+			return FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS
 
-		return LCID_ENGLISH
+		def get_lcid_eng():
+			
+			def MAKELANGID(primary_lang, sub_lang):
+				return (primary_lang & 0xff) | (sub_lang & 0xff) << 10
 
-	error_code	= ctypes.windll.kernel32.GetLastError()
-	if error_code == 0:
+			LANG_ENGLISH		= 0x09
+			SUBLANG_ENGLISH_US	= 0x01
+			LCID_ENGLISH		= MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US)
+
+			return LCID_ENGLISH
+		
+		buf = ctypes.wintypes.LPSTR()
+
+		ctypes.windll.kernel32.FormatMessageA(
+			get_fmt_msg_flag(),
+			None,
+			err_code,
+			get_lcid_eng(),
+			ctypes.byref(buf),
+			0,
+			None
+		)
+
+		err_msg = buf.value.decode('utf-8').strip()
+
+		print('')
+		print('Error: {} ({})'.format(err_msg, err_code))
+		print('')
+
+	err_code = ctypes.windll.kernel32.GetLastError()
+	if err_code == 0:
 		return
 
-	buf = ctypes.wintypes.LPSTR()
+	if err_code == 998:
+		backup()
 
-	ctypes.windll.kernel32.FormatMessageA(
-		get_fmt_msg_flag(),
-		None,
-		error_code,
-		get_lcid_eng(),
-		ctypes.byref(buf),
-		0,
-		None
-	)
-
-	error_msg = buf.value.decode('utf-8').strip()
-
-	print('')
-	print('Error: {} ({})'.format(error_msg, error_code))
-	print('')
+	print_err(err_code)
 
 def get_rand_drv_dict():
 	drv_dicts	= glob.glob('../dicts/*')
@@ -171,6 +178,6 @@ if __name__ == '__main__':
 		)
 
 		if ret_val == 0:
-			print_error()
+			handle_err()
 		
 		tries += 1
