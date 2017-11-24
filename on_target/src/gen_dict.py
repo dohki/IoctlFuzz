aaa199d22ac57ca10ae95528eba2f196dbc08a18
dohki <dohyeokkim@postech.edu>
2017-11-24 16:39:45 +0900

    Rename file_name to drv_name in gen_dict.py

--- on_target/src/gen_dict.py
+++ on_target/src/gen_dict.py
@@ -9,7 +9,7 @@ def teardown():
     for i in range(2):
         print('-' * 100)
 
-def get_file_name():
+def get_drv_name():
     DRV_EXTS     = ['386', 'drv', 'dsk', 'lan', 'nlm', 'sys', 'vxd']
     DRV_EXT_PAT  = '\.({})$'.format('|'.join(DRV_EXTS))
     
@@ -80,8 +80,8 @@ def get_ioctl_dict():
     return ioctl_dict
 
 if __name__ == '__main__':
-    file_name = get_file_name()
-    info = dict(dev_name=get_dev_name(), ioctl_dict=get_ioctl_dict())
+    drv_name    = get_drv_name()
+    info        = dict(dev_name=get_dev_name(), ioctl_dict=get_ioctl_dict())
 
-    with open('../dict/{}'.format(file_name), 'w') as f:
+    with open('../dict/{}'.format(drv_name), 'w') as f:
         f.write(json.dumps(info))
